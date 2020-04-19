98162169ab3797b2584462cecd8c738fe2b2b2ad
MaxSpiro <49620236+MaxSpiro@users.noreply.github.com>
2020-04-19 14:15:35 -0700

    almost done with todolist

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,5 +1,14 @@
 package _03_To_Do_List;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class ToDoList {
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
@@ -21,4 +30,50 @@ public class ToDoList {
 	 * 
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
+	private static ArrayList<String> list = new ArrayList<String>();
+	private final static String defaultLocation = "src/_03_To_Do_list/listFile.txt";
+	public static void main(String[] args) {
+		
+	}
+	public static void addTask() {
+		String s = JOptionPane.showInputDialog("Give me a task");
+		list.add(s);
+	}
+	public static void viewTasks() {
+		String s = "";
+		for(int i =0;i<list.size()-1;i++) s+= list.get(i)+", ";
+		JOptionPane.showMessageDialog(null, s+list.get(list.size()-1)+".");
+	}
+	public static void removeTask() {
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Which task do you want to remove?"));
+		list.remove(i-1);
+	}
+	public static void saveList() {
+		try {
+			FileWriter fw = new FileWriter("src/_03_To_Do_list/listFile.txt");
+			for(String s : list) {
+				fw.write(s+"\n");
+			}
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void loadList() {
+		String location = JOptionPane.showInputDialog("Where do you want to load the list from?");
+		if(location.equals(defaultLocation)) location = defaultLocation;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(location));
+			String line = br.readLine();
+			while(line!=null) {
+				list.add(line);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
 }
+
